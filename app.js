fc0dacb70f1bda396c5b92201f5e4e1ec1ecbcca
Iván Portillo <ivan.portillo@kuehne-nagel.com>
2021-03-27 07:40:58 +0100

    primeras instrucciones

--- app.js
+++ app.js
@@ -4,8 +4,47 @@ new Vue({
     data() {
         return {
             name: 'Bitcoin',
+            symbol: 'BTC',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 9,
+            changePercent: 4,
+            value: 0,
+            color: 'f4f4f4',
+            price: 7000,
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 3000 },
+                { day: 'Miércoles', value: 5400 },
+                { day: 'Jueves', value: 4999 },
+                { day: 'Viernes', value: 6011 },
+                { day: 'Sábado', value: 7000 },
+                { day: 'Domingo', value: 8525 },
+            ],
+            showPrices: false,
         }
-    }
+    },
+    computed: {
+        title() {
+            return `${this.name} - ${this.symbol}`
+        },
+
+        convertedValue() {
+            if (!this.value) {
+                return 0
+            }
+            return this.value / this.price
+        },
+    },
+
+    watch: {
+        showPrices(newVal, oldVal) {
+            console.log(newVal, oldVal)
+        }
+    },
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
+
+            this.color = this.color.split('').reverse().join('')
+        }
+    },
 })
